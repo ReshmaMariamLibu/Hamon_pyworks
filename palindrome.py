32f638e0e8d47b71abf1ae33bae8bc25eb2fcb5e
Reshma Mariam Libu <reshmamariamlibu@gmail.com>
2024-05-21 14:03:25 +0530

    Checked a string is palindrome or not using function

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,10 @@
 def palindrome(s):
-#    s=s.lower() #to convert upercase character to lowercase
-#    r=s[::-1] #to reverse the string
-#    if s==r: 
-#     print("palindrome")
-#    else:
-#     print("not a palindrome")
-
-  w=len(s)
-  for w in range(w,-1,-1):
-     print(w)
+   s=s.lower() #to convert upercase character to lowercase
+   r=s[::-1] #to reverse the string
+   if s==r: # check reversed string with
+    print("palindrome")
+   else:
+    print("not a palindrome")
 
 
 
