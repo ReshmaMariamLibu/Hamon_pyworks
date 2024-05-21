10dc49f090c8f41e6e58d6f45b5d4d2dbfc21c56
Reshma Mariam Libu <reshmamariamlibu@gmail.com>
2024-05-21 13:59:34 +0530

    palindrome edited

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,14 @@
 def palindrome(s):
-   s=s.lower() #to convert upercase character to lowercase
-   r=s[::-1] #to reverse the string
-   if s==r: 
-    print("palindrome")
-   else:
-    print("not a palindrome")
+#    s=s.lower() #to convert upercase character to lowercase
+#    r=s[::-1] #to reverse the string
+#    if s==r: 
+#     print("palindrome")
+#    else:
+#     print("not a palindrome")
+
+  w=len(s)
+  for w in range(w,-1,-1):
+     print(w)
 
 
 
